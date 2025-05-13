c5251ccf4e9242b25c07be023fdab2735d26226a
Bowen Yuan <byuan105@gmail.com>
2025-05-12 20:21:52 -0400

    added timer for consistency

--- main.py
+++ main.py
@@ -5,27 +5,33 @@ from game_world import World
 
 pygame.init()
 
-screen_width = 1000
+screen_width = 1800
 screen_height = 1000
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Hardest Game")
 
 tile_size = 200
+clock = pygame.time.Clock()
+fps = 60
 
-world_data = [
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 0, 1],
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 0, 1],
-    [0, 1, 0, 1, 0],
+level1_data = [
+    [0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [0, 1, 0, 1, 0, 1, 0, 1, 0],
 ]
 
-world = World(world_data, tile_size)
+level1 = World(level1_data, tile_size)
+
+deaths = 0
 
 run = True
 while run:
-    world.draw()
+    clock.tick(fps)
+    screen.fill((255, 255, 255))
+    level1.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
