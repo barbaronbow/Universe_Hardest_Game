8f78bded9e3feae90953b25884f1e12605c950ad
barbaronbow <113142230+barbaronbow@users.noreply.github.com>
2025-05-30 10:48:24 -0400

    Update game_world.py

--- game_world.py
+++ game_world.py
@@ -11,18 +11,28 @@ class World:
         self.tile_list = []
         self.tile_size = tile_size
 
-        wall_img = pygame.image.load("img/wall.jpg")
+        wall_img = pygame.image.load("wall.jpg")
+        checkpoint_img = pygame.image.load("checkpoint.jpg")
 
         row_count = 0
         for row in data:
             col_count = 0
             for tile in row:
-                if tile == 1:
+                if tile == 1: # Wall
                     img = pygame.transform.scale(wall_img, (tile_size, tile_size))
                     img_rect = img.get_rect()
                     img_rect.x = col_count * tile_size
                     img_rect.y = row_count * tile_size
-                    tile = (img, img_rect)
+                    collidable = True
+                    tile = (img, img_rect, collidable)
+                    self.tile_list.append(tile)
+                if tile == 2: # Checkpoint
+                    img = pygame.transform.scale(checkpoint_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    collidable = False
+                    tile = (img, img_rect, collidable)
                     self.tile_list.append(tile)
                 col_count += 1
             row_count += 1
