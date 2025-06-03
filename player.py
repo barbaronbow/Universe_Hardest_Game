0901067735dd6d38ef8571d75016fd19040e63e6
Bowen Yuan <byuan105@gmail.com>
2025-06-02 21:51:15 -0400

    added arrow key movement

--- player.py
+++ player.py
@@ -8,52 +8,58 @@ screen_height = 1000
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 
-
 class Player:
     def __init__(self, x, y, size):
-        
+
         img = pygame.image.load("player.png")
         self.image = pygame.transform.scale(img, (size * 5 / 8, size * 5 / 8))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        
+
         self.spawnpoint = (x, y)
         self.width = self.image.get_width()
         self.height = self.image.get_height()
 
-
     def update(self, level_data: World):
         dx = 0
         dy = 0
-        
+
         key = pygame.key.get_pressed()
-        if key[pygame.K_a]:
+        if key[pygame.K_a] or key[pygame.K_LEFT]:
             dx -= 1
 
-        if key[pygame.K_d]:
+        if key[pygame.K_d] or key[pygame.K_RIGHT]:
             dx += 1
 
-        if key[pygame.K_w]:
+        if key[pygame.K_w] or key[pygame.K_UP]:
             dy -= 1
 
-        if key[pygame.K_s]:
+        if key[pygame.K_s] or key[pygame.K_DOWN]:
             dy += 1
-        
-        
+
         for tile in level_data.tile_list:
-            #check for collision in x direction
+            # check for collision in x direction
             if tile[2]:
-                if tile[1].colliderect(self.rect.x + dx, self.rect.y, self.width, self.height):
+                if tile[1].colliderect(
+                    self.rect.x + dx, self.rect.y, self.width, self.height
+                ):
                     dx = 0
-                #check for collision in y direction
-                if tile[1].colliderect(self.rect.x, self.rect.y + dy, self.width, self.height):
+                # check for collision in y direction
+                if tile[1].colliderect(
+                    self.rect.x, self.rect.y + dy, self.width, self.height
+                ):
                     dy = 0
-                    
+
             else:
-                if tile[1].colliderect(self.rect.x, self.rect.y, self.width, self.height):
-                    self.spawnpoint = (self.rect.x + self.width, self.rect.y + self.height)
-        
+                if tile[1].colliderect(
+                    self.rect.x, self.rect.y, self.width, self.height
+                ):
+                    self.spawnpoint = (
+                        self.rect.x + self.width,
+                        self.rect.y + self.height,
+                    )
+
         self.rect.x += dx
         self.rect.y += dy
         screen.blit(self.image, self.rect)
@@ -61,8 +67,8 @@ class Player:
     def respawn(self):
         self.rect.x = self.spawnpoint[0]
         self.rect.y = self.spawnpoint[1]
-        
-        
+
+
 class Obstacle(Player):
     def __init__(self, x, y, size):
         super().__init__(x, y, size)
@@ -71,8 +77,6 @@ class Obstacle(Player):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-    
 
-        
     def update(self):
         screen.blit(self.image, self.rect)
